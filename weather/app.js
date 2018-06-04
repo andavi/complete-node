66e09d2e05697112ffb0214db22dd7ede9aa3d0a
Andrew Davis <andrew.bradley.davis@gmail.com>
2018-06-04 15:07:57 -0400

    connect to Dark Sky

--- weather/app.js
+++ weather/app.js
@@ -1,25 +1,39 @@
-const yargs = require('yargs');
+// const yargs = require('yargs');
 
-const geocode = require('./geocode/geocode');
+// const geocode = require('./geocode/geocode');
 
-const argv = yargs
-	.options({
-		a: {
-			demand: true,
-			alias: 'address',
-			describe: 'Address to fetch weather for',
-			string: true
-		}
-	})
-	.help()
-	.alias('help', 'h')
-	.argv;
+// const argv = yargs
+// 	.options({
+// 		a: {
+// 			demand: true,
+// 			alias: 'address',
+// 			describe: 'Address to fetch weather for',
+// 			string: true
+// 		}
+// 	})
+// 	.help()
+// 	.alias('help', 'h')
+// 	.argv;
 
-geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-	if (errorMessage) {
-		console.log(errorMessage);
+// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+// 	if (errorMessage) {
+// 		console.log(errorMessage);
+// 	} else {
+// 		console.log(JSON.stringify(results, undefined, 2));
+// 	}
+// });
+
+// 0f0c82f975c9ff52605f666185b709a9
+
+const request = require('request');
+
+request({
+	url: `https://api.darksky.net/forecast/0f0c82f975c9ff52605f666185b709a9/28.1988979,-80.7879407`,
+	json: true
+}, (error, response, body) => {
+	if (!error && response.statusCode === 200) {
+		console.log(`Temperature now: ${body.currently.temperature}.`);
 	} else {
-		console.log(JSON.stringify(results, undefined, 2));
+		console.log('Unable to fetch weather.');
 	}
 });
-
--- weather/geocode/geocode.js
+++ weather/geocode/geocode.js
@@ -15,7 +15,7 @@ geocodeAddress = (address, callback) => {
 			callback(undefined, {
 				address: body.results[0].formatted_address,
 				latitude: body.results[0].geometry.location.lat,
-				longitude: body.results[0].geometry.location.lat
+				longitude: body.results[0].geometry.location.lng
 			});
 		}
 	});
